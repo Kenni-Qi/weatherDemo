c6f0c3e24f6530318a00320d4a6fcbe6c683818e
qihaokai <qihaokai@9.112.74.20>
2021-04-27 14:03:39 +0800

    Update JUnit Test 20210427 qhkqi

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import java.io.IOException;
+import java.text.ParseException;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplicationTests {
+import com.example.demo.entity.WeatherEntity;
+import com.example.demo.service.impl.TestServiceImpl;
+import com.google.gson.Gson;
+
+@SpringBootTest(classes = DemoApplication.class)
+public class DemoApplicationTests {
+	@Autowired
+	TestServiceImpl testServiceImpl;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws IOException, ParseException {
+		String cityValue = "Sydney";
+		WeatherEntity entity = testServiceImpl.getWeather(cityValue);
+		Gson gson = new Gson();
+		String toPrintStr = gson.toJson(entity);
+		System.out.println(toPrintStr);
+	    System.out.println("TEST SUCCESSFULLY");
 	}
 
 }
